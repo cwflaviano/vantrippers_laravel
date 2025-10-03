67277e04510d92f7790f767084a3ee8e4f711dff
JPSR-git <paulo.emailmehere@gmail.com>
2025-10-03 08:16:30 +0800

    fixed add database connection and employee fields to User

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,14 +14,39 @@ class User extends Authenticatable implements MustVerifyEmail
     use HasFactory, Notifiable, HasApiTokens;
 
     /**
+     * The database connection that should be used by the model.
+     *
+     * @var string
+     */
+    protected $connection = 'vantripper_db';
+
+    /**
      * The attributes that are mass assignable.
      *
      * @var list<string>
      */
     protected $fillable = [
+        'emp_id',
         'name',
+        'first_name',
+        'middle_name',
+        'last_name',
+        'gender',
+        'address',
+        'birthdate',
+        'age',
         'email',
+        'email_verified_at',
         'password',
+        'contact',
+        'position',
+        'date_of_joining',
+        'type_of_contract',
+        'department_id',
+        'role_id',
+        'status',
+        'user_delete',
+        'user_archived',
         'is_admin',
         'is_approved',
     ];
